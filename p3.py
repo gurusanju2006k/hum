8fb38218f510c09a6e652a860abeff26676f8ed9
gurusanju2006k <gurusanju2006k@gmail.com>
2025-11-23 20:36:25 +0530

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Sum of numbers from 0 to 999
+print(np.sum(np.arange(1000)))
+
+# Vectorized subtraction
+print(2 * np.array([10.2, 3.5, -0.9]) - np.array([8.2, 3.5, 6.5]))
+
+# Dot product
+print(np.dot(np.array([1, -3, 4]), np.array([2, 0, 1])))
